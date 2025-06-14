2782f3db3792ee79e2ea9a0e2a75fe1749e87420
Vignan <vignanreddy.thipparthi09@gmail.com>
2025-06-14 17:04:28 -0500

    Done Design-3

---
+++ NestedIterator.java
@@ -0,0 +1,33 @@
+// Time complexity :O(1);
+//Space Complexity : O(n);
+public class NestedIterator implements Iterator<Integer> {
+    private Deque<NestedInteger> stack;
+
+    public NestedIterator(List<NestedInteger> nestedList) {
+        stack = new ArrayDeque<>();
+        for (int i = nestedList.size() - 1; i >= 0; i--) {
+            stack.push(nestedList.get(i));
+        }
+    }
+
+    @Override
+    public Integer next() {
+        return stack.pop().getInteger();
+    }
+
+    @Override
+    public boolean hasNext() {
+        while (!stack.isEmpty()) {
+            NestedInteger top = stack.peek();
+            if (top.isInteger()) {
+                return true;
+            }
+            stack.pop();
+            List<NestedInteger> nestedList = top.getList();
+            for (int i = nestedList.size() - 1; i >= 0; i--) {
+                stack.push(nestedList.get(i));
+            }
+        }
+        return false;
+    }
+}
